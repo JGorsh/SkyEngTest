e3877ddfda40c1f4ba75205820693dc322760ad8
asgor <alexmilesjg@gmail.com>
2023-08-21 17:35:20 +0500

    refactor

--- src/main/java/com/example/model/dto/UpdateMailingRequestDto.java
+++ src/main/java/com/example/model/dto/UpdateMailingRequestDto.java
@@ -14,16 +14,5 @@ public class UpdateMailingRequestDto {
 
     @NotNull
     private UUID uuid;
-    private MailType mailType;
-    private Integer mailIndex;
-    private String recipientName;
-    private String recipientAddress;
-//
-//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
-//    private Instant sentedTime;
-//
-//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
-//    private Instant receivedTime;
-
     private PostOffice postOffice;
 }
