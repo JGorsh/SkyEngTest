024020f5905734d633138a1cc93210914b0c9670
asgor <alexmilesjg@gmail.com>
2023-08-25 10:22:22 +0500

    add test

--- src/main/java/com/example/SkyEngTestApplication.java
+++ src/main/java/com/example/SkyEngTestApplication.java
@@ -2,12 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SkyEngTestApplication {
+public class SkyEngTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SkyEngTestApplication.class, args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SkyEngTestApplication.class);
+    }
 }
